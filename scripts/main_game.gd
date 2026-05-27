2dc61a86be1f9e19f4d05e496f469c12eb1a8825
simonM-b <slivwrm88@gmail.com>
2026-05-27 08:58:24 -0400

    added cell left/reight up/down detection

--- scripts/main_game.gd
+++ scripts/main_game.gd
@@ -12,6 +12,7 @@ var cursorColor = Color(Color.BLACK,0.3)
 var playState = "no cell action"
 
 var frame = 0
+var offset:int = 0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -21,11 +22,11 @@ func _ready() -> void:
 
 func _draw():
 	draw_rect(Rect2(0, 0, size.x, size.y), Color.GREEN)
-	draw_rect(Rect2(drawPosition.x-0.5, drawPosition.y-0.5, 1, 1), cursorColor)
-	
+	draw_rect(Rect2(drawPosition.x-offset, drawPosition.y-offset, 1, 1), cursorColor)
+
 	for cell in drawCellArray:
-		draw_rect(Rect2(cell.x-0.5, cell.y-0.5, 1, 1), Color.BLACK)
-			
+		draw_rect(Rect2(cell.x-offset, cell.y-offset, 1, 1), Color.BLACK)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -42,52 +43,45 @@ func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
-			drawCellArray.append(event.position)
+			drawCellArray.append(Vector2(roundf(event.position.x),roundf(event.position.y)))
 			queue_redraw()
-		
-	
+
+
 	if event is InputEventKey and event.pressed:
 		if event.keycode == KEY_ESCAPE:
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	
+
 	if event.is_action_pressed("start"):
 		frameTimer.start()
 		playState = "run cell"
-	
+
 	if event.is_action_pressed("stop"):
 		frameTimer.stop()
 		playState = "no cell action"
 
 func nextFrame():
-	print(frame)
+	print("FRAME NUMB: ",frame)
 	for cell in drawCellArray:
 		for NeighborCell in drawCellArray:
-			#over by one +x same y 
+			#over by one +x same y
 			#over by -x same y
 			#over by +x +y
-			#over by +x -y 
+			#over by +x -y
 			#over by -x +y
 			#over by -x -y
 			#same x +y
 			#same x -y
-			if cell.position.x+1 == NeighborCell.position.x:
+			#print("CELL x:",cell.x," Y:",cell.y)
+			if cell.x+1 == NeighborCell.x: #alive cell to the right
 				print("cell +1x over")
-			elif cell.position.x-1 == NeighborCell.position.x:
+			elif cell.x-1 == NeighborCell.x: #alive cell to the left
 				print("cell -1x over")
-			elif cell.position.y-1 == NeighborCell.position.y:
+			elif cell.y-1 == NeighborCell.y: #alive cell above
 				print("cell -1y over")
-			elif cell.position.y+1 == NeighborCell.position.y:
+			elif cell.y+1 == NeighborCell.y: #alive cell left
 				print("cell +1y over")
-		
+
 
 func _on_next_frame_timeout() -> void:
 	frame += 1
 	nextFrame()
-	
-	
-	
-	
-	
-	
-	
-	
