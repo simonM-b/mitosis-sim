44dcd2fc648ccb169ac11869d499a6aaa7069244
simonM-b <slivwrm88@gmail.com>
2026-05-26 21:32:18 -0400

    added cell spawning

---
+++ docs/how_to_use.md
@@ -0,0 +1,15 @@
+# Basic keyboard shortcuts:
+
+the buttons that you need to click to perform each action will be capitalized and/or italicized
+
+Note: cells are the thing that will get simulated in the simulation.
+
+## Start simulation: *SPACE*
+
+## Stop simulation: *CTRL* (or *COMMAND* on mac)
+
+## Delete cells: hold *SHIFT*
+
+## Get cursor: ESC
+
+# Rules:
--- scripts/main_game.gd
+++ scripts/main_game.gd
@@ -6,14 +6,19 @@ extends Node2D
 const cellPreload = preload("res://scenes/cell.tscn")
 var initDraw = true
 var drawPosition:Vector2 = Vector2(0,0)
+var drawCellArray = []
+var cursorColor = Color(Color.BLACK,0.3)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	OS.shell_open(ProjectSettings.globalize_path("res://docs/"))
 
 func _draw():
 	draw_rect(Rect2(0, 0, size.x, size.y), Color.GREEN)
-	draw_rect(Rect2(drawPosition.x-0.5, drawPosition.y-0.5, 1, 1), Color.BLACK)
+	draw_rect(Rect2(drawPosition.x-0.5, drawPosition.y-0.5, 1, 1), cursorColor)
+	
+	for cell in drawCellArray:
+		draw_rect(Rect2(cell.x-0.5, cell.y-0.5, 1, 1), Color.BLACK)
 			
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -29,7 +34,11 @@ func _input(event):
 		drawPosition = event.position
 		queue_redraw()
 	if event is InputEventMouseButton:
-		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+			drawCellArray.append(event.position)
+			queue_redraw()
+		
 	
 	if event is InputEventKey and event.pressed:
 		if event.keycode == KEY_ESCAPE:
